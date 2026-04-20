571e12659ce825b5a2d6111d0b0f11e0a1aa4eb4
Amine Elhachimi <amine2005.elhachimi@gmail.com>
2026-04-20 10:46:08 +0100

    update

---
+++ my_note.py
@@ -0,0 +1,25 @@
+import numpy
+
+
+# # MODE
+# speed = [99,86,87,88,86,103,87,94,78,77,85,86]
+# x = numpy.median(speed)
+# print(x)
+# # = 86 Most common
+
+
+# # MEDIAN
+# speed = [99,86,87,88,111,86,103,87,94,78,77,85,86]
+# x = numpy.median(speed)
+# print(x)
+# # after sort list given the centreer number of list if in center tow numbers is returned (86 + 87) / 2 = 86.5
+
+
+## MEAN
+# speed = [99,86,87,88,111,86,103,87,94,78,77,85,86]
+# x = numpy.mean(speed)
+# print(x)
+## it returned averge number of liste soo ((99+86+87+88+111+86+103+87+94+78+77+85+86) / 13 = 89.77)
+
+
+
