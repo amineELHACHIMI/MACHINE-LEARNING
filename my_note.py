f1c8ddb50e98a1251a9db944bff8faea3ea26925
amineELHACHIMI <amine2005.elhachimi@gmail.com>
2026-04-22 19:57:42 +0100

    update my note

--- my_note.py
+++ my_note.py
@@ -1,6 +1,5 @@
 import numpy
 
-
 # # MODE
 # speed = [99,86,87,88,86,103,87,94,78,77,85,86]
 # x = numpy.median(speed)
@@ -20,6 +19,3 @@ import numpy
 # x = numpy.mean(speed)
 # print(x)
 ## it returned averge number of liste soo ((99+86+87+88+111+86+103+87+94+78+77+85+86) / 13 = 89.77)
-
-
-
