9d9e90d8019f8fef411aa946230906701b95c098
amineELHACHIMI <amine2005.elhachimi@gmail.com>
2026-04-25 23:23:54 +0100

    tisting code

--- my_note.py
+++ my_note.py
@@ -1,10 +1,10 @@
-import numpy
+import numpy as np
 
 # # MODE
 # speed = [99,86,87,88,86,103,87,94,78,77,85,86]
 # x = numpy.median(speed)
 # print(x)
-# # = 86 Most common
+# # = 86 Most common    
 
 
 # # MEDIAN
@@ -19,3 +19,7 @@ import numpy
 # x = numpy.mean(speed)
 # print(x)
 ## it returned averge number of liste soo ((99+86+87+88+111+86+103+87+94+78+77+85+86) / 13 = 89.77)
+
+# NumPy routines which allocate memory and fill with user specified values
+a = np.array([5,4,3,2, 6]);  print(f"np.array([5,4,3,2]):  a = {a},     a shape = {a.shape}, a data type = {a.dtype}")
+a = np.array([5.,4,3,2]); print(f"np.array([5.,4,3,2]): a = {a}, a shape = {a.shape}, a data type = {a.dtype}")
